406d87ac609a80c493db7d2ea784c7e4cf655224
rupa- <s24C1020NS@s.chibakoudai.jp>
2025-12-31 06:37:33 +0900

    modifing test

--- test/test.bash
+++ test/test.bash
@@ -12,4 +12,6 @@ source $dir/.bashrc
 
 timeout 10 ros2 launch ros2_chat chat.launch.py > /tmp/ros2_chat.log 2>&1
 
-cat /tmp/ros2_chat.log | grep 'chat_listener'
+grep 'chat_listener' /tmp/ros2_chat.log || exit 1
+
+grep 'chat_talker' /tmp/ros2_chat.log || exit 1
